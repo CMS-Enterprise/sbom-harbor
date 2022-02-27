90803686251473d69b2ecd8f03acf84d53952239
qtpeters <quinn.t.peters@gmail.com>
2022-02-27 11:41:07 -0500

    Adding cdk from other project

---
+++ cdk/test/aws-cdk-sbom-infra.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as AwsCdkSbomInfra from '../lib/aws-cdk-sbom-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-sbom-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkSbomInfra.AwsCdkSbomInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
