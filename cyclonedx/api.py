7e0334c06be81117cc7d41896c399cfb79ce5f17
qtpeters <quinn.t.peters@gmail.com>
2022-02-28 08:30:54 -0500

    Worked in the core cyclonedx code

--- cyclonedx/api.py
+++ cyclonedx/api.py
@@ -1,24 +1,57 @@
-import jsonschema
 import boto3
 import os
 
+from jsonschema.exceptions import ValidationError
+from urllib import response
 from uuid import uuid4
-from json import dumps
+from json import loads, dumps
+
+from cyclonedx.core import CycloneDxCore
 
 def lambda_handler(event, context):
 
+    # Extract the body from the event
+    bom_obj = event
+
+    # Get the bucket name from the environment variable
+    # This is set during deployment
     bucket_name = os.environ["SBOM_BUCKET_NAME"]
     print("Bucket name from env(SBOM_BUCKET_NAME): %s" % bucket_name)
 
+    # Generate the name of the object in S3
     key = "aquia-%s" % uuid4()
+    print("Putting object in S3 with key: %s" % key)
 
-    json = dumps(event)
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket(bucket_name)
-    bytes = bytearray(json, "utf-8")
-    bucket.put_object(Key=key, Body=bytes)
-    
-    return {
+    # Create an instance of the Python CycloneDX Core
+    core = CycloneDxCore()
+
+    # Create a response object to add values to.
+    response_obj = {
         'statusCode': 200,
-        'body': json
+        'body': {
+            'valid': True,
+            's3BucketName': bucket_name,
+            's3ObjectKey': key
+        }
     }
+
+    try:
+
+        # Validate the BOM here
+        core.validate(bom_obj)
+
+        # Get S3 Bucket
+        s3 = boto3.resource('s3')
+        bucket = s3.Bucket(bucket_name)
+
+        # Actually put the object in S3
+        bytes = bytearray(dumps(bom_obj), "utf-8")
+        bucket.put_object(Key=key, Body=bytes)
+        response_obj['body']['s3ObjectKey'] = key
+
+    except ValidationError as e:
+        response_obj['statusCode'] = 400
+        response_obj['body'] = str(e)
+
+
+    return response_obj
--- cyclonedx/core.py
+++ cyclonedx/core.py
@@ -1,6 +1,8 @@
 import importlib.resources as pr
 import cyclonedx.schemas as schemas 
-
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+from json import loads
 
 class CycloneDxCore:
     
@@ -14,3 +16,26 @@ class CycloneDxCore:
     def get_schema(self, version) -> str:
         return self.sbom_schemas[version]
     
+    def validate(self, bom_obj: dict) -> None:
+
+        try:
+            bom_format = bom_obj["bomFormat"]
+        except KeyError:
+            raise ValidationError('Missing "bomFormat" key, is this a BOM you are trying to send?')
+
+        try:
+            schema_version = bom_obj["specVersion"]
+        except KeyError:
+            raise ValidationError('Missing "specVersion" key, is this a BOM you are trying to send?')
+
+
+        if bom_format != "CycloneDX":
+            raise ValidationError("SBOM-API only supports CycloneDX SBOM Formats")
+
+        if schema_version not in self.sbom_schemas:
+            raise ValidationError("CycloneDX Schema Version %s is not supported" % schema_version)
+
+        schema_json = self.sbom_schemas[schema_version]
+        schema_obj = loads(schema_json)
+
+        validate(instance=bom_obj, schema=schema_obj)
