589de462124e44343b0ae53bb80c11c55abaa2fc
Derek Strickland <derek.strickland@gmail.com>
2022-12-21 15:42:10 -0500

    add generator build script; create test context

---
+++ openapi/build.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+TESTS=0
+CLIENT=0
+SERVER=0
+
+VERSION="v6.2.1"
+DOCKER_IMAGE="openapitools/openapi-generator-cli:${VERSION}"
+GENERATE="docker run --rm --volume "$(PWD):/local" ${DOCKER_IMAGE} batch --clean"
+
+while getopts "tcs" arg; do
+  case "${arg}" in
+    t) TESTS=1
+       ;;
+    c) CLIENT=1
+       ;;
+    s) SERVER=1
+       ;;
+    ?)
+      echo "Invalid option: -$OPTARG"
+      ;;
+  esac
+done
+
+echo "
+    TESTS: $TESTS
+    CLIENT: $CLIENT
+    SERVER: $SERVER
+    "
+
+if [[ $TESTS == 1 ]]; then
+#  docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate \
+#    -i /local/openapi.yaml \
+#    -g k6 \
+#    -o /local/.openapi-generator/
+    echo "Generating Tests"
+    $GENERATE /local/openapi/tests/config.yaml
+fi
+
+if [[ $CLIENT == 1 ]]; then
+  echo "Generating API Client"
+  $GENERATE /local/openapi/client/config.yaml
+fi
+
+if [[ $SERVER == 1 ]]; then
+  echo "Generating API Server"
+  $GENERATE /local/openapi/server/config.yaml
+fi
