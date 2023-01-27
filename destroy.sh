aa961e78416afe6990eb04fa1c7925bd3ebd427c
Richard Jones <richard.jones@aquia.io>
2023-01-27 09:57:12 -0700

    dissallow dev and prod; ignore s3 delete object errors

--- destroy.sh
+++ destroy.sh
@@ -1,8 +1,39 @@
 #!/usr/bin/env bash
 # set -euxo pipefail
 
-source ./deploy-preamble.sh
 
+if [[ $ENVIRONMENT == "dev" || $ENVIRONMENT == "prod" ]]; then
+  echo "
+
+███████▓█████▓▓╬╬╬╬╬╬╬╬▓███▓╬╬╬╬╬╬╬▓╬╬▓█
+████▓▓▓▓╬╬▓█████╬╬╬╬╬╬███▓╬╬╬╬╬╬╬╬╬╬╬╬╬█
+███▓▓▓▓╬╬╬╬╬╬▓██╬╬╬╬╬╬▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+████▓▓▓╬╬╬╬╬╬╬▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+███▓█▓███████▓▓███▓╬╬╬╬╬╬▓███████▓╬╬╬╬▓█
+████████████████▓█▓╬╬╬╬╬▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬█
+███▓▓▓▓▓▓▓╬╬▓▓▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+████▓▓▓╬╬╬╬▓▓▓▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+███▓█▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+█████▓▓▓▓▓▓▓▓█▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+█████▓▓▓▓▓▓▓██▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+█████▓▓▓▓▓████▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+████▓█▓▓▓▓██▓▓▓▓██╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+████▓▓███▓▓▓▓▓▓▓██▓╬╬╬╬╬╬╬╬╬╬╬╬█▓╬▓╬╬▓██
+█████▓███▓▓▓▓▓▓▓▓████▓▓╬╬╬╬╬╬╬█▓╬╬╬╬╬▓██
+█████▓▓█▓███▓▓▓████╬▓█▓▓╬╬╬▓▓█▓╬╬╬╬╬╬███
+██████▓██▓███████▓╬╬╬▓▓╬▓▓██▓╬╬╬╬╬╬╬▓███
+███████▓██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬████
+███████▓▓██▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓████
+████████▓▓▓█████▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█████
+█████████▓▓▓█▓▓▓▓▓███▓╬╬╬╬╬╬╬╬╬╬╬▓██████
+██████████▓▓▓█▓▓▓╬▓██╬╬╬╬╬╬╬╬╬╬╬▓███████
+███████████▓▓█▓▓▓▓███▓╬╬╬╬╬╬╬╬╬▓████████
+██████████████▓▓▓███▓▓╬╬╬╬╬╬╬╬██████████
+███████████████▓▓▓██▓▓╬╬╬╬╬╬▓███████████"
+  exit
+fi
+
+source ./deploy-preamble.sh
 echo "
 This will DESTROY the $ENVIRONMENT environment!
 This includes deleting all objects in S3 buckets, and deleting all cloudformation stacks!
@@ -16,14 +47,14 @@ fi
 
 sbomBucket="$ENVIRONMENT-harbor-sbom-uploads-enrichment-$AWS_ACCOUNT_ID-$AWS_REGION_SHORT"
 echo "Deleting objects in $sbomBucket"
-aws s3 rm s3://$sbomBucket --recursive
+aws s3 rm s3://$sbomBucket --recursive 2> /dev/null
 
 assetsBucket="$ENVIRONMENT-harbor-web-assets-$AWS_ACCOUNT_ID-$AWS_REGION_SHORT"
 echo "Deleting objects in $assetsBucket"
-aws s3 rm s3://$assetsBucket --recursive
+aws s3 rm s3://$assetsBucket --recursive 2> /dev/null
 
 dtLogsBucket="$ENVIRONMENT-dt-alb-logs-$AWS_ACCOUNT_ID-$AWS_REGION_SHORT"
 echo "Deleting objects in $dtLogsBucket"
-aws s3 rm s3://$dtLogsBucket --recursive
+aws s3 rm s3://$dtLogsBucket --recursive 2> /dev/null
 
 ION_CHANNEL_TOKEN="none" cdk destroy --role-arn $CDK_ROLE_ARN --concurrency 6 $ENVIRONMENT-harbor-shared-resources-$AWS_REGION_SHORT $ENVIRONMENT-harbor-user-management-$AWS_REGION_SHORT $ENVIRONMENT-harbor-backend-$AWS_REGION_SHORT $ENVIRONMENT-harbor-frontend-$AWS_REGION_SHORT $ENVIRONMENT-harbor-enrichment-$AWS_REGION_SHORT
