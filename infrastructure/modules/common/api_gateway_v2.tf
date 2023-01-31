d17bcd5e19d106ee489093bb3009346057b2f513
Richard Jones <richard.jones@aquia.io>
2023-01-31 14:15:33 -0700

    removed terraform

--- infrastructure/modules/common/api_gateway_v2.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_apigatewayv2_api" "harbor_api_gateway" {
-  name                       = "example-websocket-api"
-  protocol_type              = "WEBSOCKET"
-  route_selection_expression = "$request.body.action"
-}
