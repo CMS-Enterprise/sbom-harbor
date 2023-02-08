30854533db4e204f130fcb0f10d83567dc411605
Sinan Bolel <1915925+sbolel@users.noreply.github.com>
2023-02-07 19:45:56 -0500

    build(ui): remove execSync of preamble script

--- ui/packages/sbom/craco.prebuild.js
+++ ui/packages/sbom/craco.prebuild.js
@@ -1,17 +1,14 @@
 /**
- * Craco config that overrides the default webpack config.
+ * This file is executed before the CRA (craco.config.js) build process.
+ * It is used to set the global app config from environment variables.
+ * The config is then passed to the app via the webpack EnvironmentPlugin.
  */
-const { resolve } = require('path')
-const { execSync } = require('child_process')
 
 // mock environment variables for the global app config during tests
 if (process.env.NODE_ENV === 'test') {
   process.env.CF_DOMAIN = 'localhost:3000'
   process.env.USER_POOL_ID = 'us-east-1_123456789'
   process.env.USER_POOL_CLIENT_ID = '1234567890123456789012'
-} else {
-  const preamblePath = resolve(__dirname, '../../../deploy-preamble.sh')
-  execSync(`source ${preamblePath}`, { stdio: 'inherit', encoding: 'utf8' })
 }
 
 // set the global app config from environment variables
@@ -26,7 +23,7 @@ const CONFIG = {
 }
 
 // Output the global app config to the shell during builds
-// except when running tests to avoid polluting the test output.
 console.log('UI CONFIG:', CONFIG, '\n')
 
+// export the global app config for use by the webpack EnvironmentPlugin
 module.exports = CONFIG
