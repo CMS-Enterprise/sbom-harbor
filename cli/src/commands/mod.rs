5e5f0aeb1d1779c8bd8663796289e9d94daa1e7a
qtpeters <quinn.t.peters@gmail.com>
2023-03-29 11:00:24 -0400

    feat(gh_provider): moving to new branch

--- cli/src/commands/mod.rs
+++ cli/src/commands/mod.rs
@@ -3,3 +3,12 @@ pub mod enrich;
 /// Contains the types and functions to support the SBOM Command.
 pub mod sbom;
 
+
+/// Extracts the value of an environment variable
+///
+fn get_env_var(variable_name: &str) -> Option<String> {
+    return match env::var(variable_name) {
+        Ok(v) => Some(v),
+        Err(e) => None,
+    };
+}
