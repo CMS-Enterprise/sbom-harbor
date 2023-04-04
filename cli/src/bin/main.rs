91090d8af1697d0a66588003e7b7b2eabbcb1f2a
Derek Strickland <derek.strickland@gmail.com>
2023-04-04 15:03:57 -0400

    feat: set up Clap CLI structure

--- cli/src/bin/main.rs
+++ cli/src/bin/main.rs
@@ -1,109 +1,16 @@
-use clap::{Arg, ArgAction, Command, ArgMatches};
-use std::env;
-
-fn get_matches() -> ArgMatches {
-    Command::new("harbor-cli")
-    .about("SBOM Harbor Runtime CLI")
-    .version("0.0.1")
-    .subcommand_required(false)
-    .arg_required_else_help(true)
-    .author("SBOM Harbor Team")
-    .arg(
-        Arg::new("account")
-            .short('a')
-            .required(true)
-            .long("account")
-            .help("aws account id")
-            .action(ArgAction::Set)
-            .num_args(1),
-    )
-    .arg(
-        Arg::new("env")
-            .short('e')
-            .required(true)
-            .long("env")
-            .help("environment, ephemeral or permanent.")
-            .action(ArgAction::Set)
-            .num_args(1),
-    )
-    .subcommand(
-        Command::new("start")
-            .about("Start a Pilot Execution")
-    )
-    .get_matches()
-}
-
-fn get_gh_token() -> String {
-    match env::var("GH_FETCH_TOKEN") {
-        Ok(v) => v,
-        Err(e) => panic!("$GH_FETCH_TOKEN is not set ({})", e)
-    }
-}
-
-#[allow(dead_code)]
-fn get_cf_domain() -> String {
-    match env::var("CF_DOMAIN") {
-        Ok(v) => v,
-        Err(e) => panic!("$CF_DOMAIN is not set ({})", e)
-    }
-}
-
-async fn list_repos() {
-
-    let _token = get_gh_token();
-
-
-    // curl -X GET -u <UserName>:<Generated-Token>https://api.github.com/orgs/<Org-Name>/repos | grep -w clone_url
-
-    // let project: Option<Project> =
-    //     post(create_project_url.as_str(), token.as_str(), Some(project)).await?;
-    // let team: Option<Team> = get(url.as_str(), token.as_str(), None::<Team>).await?;
-
-    // Ok(team.unwrap());
-    // let octocrab = octocrab::OctocrabBuilder::new()
-    //     .personal_token(token)
-    //     .build()
-    //     .unwrap();
-    //
-    // let current_page = octocrab
-    //     .orgs("cmsgov")
-    //     .list_repos()
-    //     .repo_type(params::repos::Type::Sources)
-    //     .sort(params::repos::Sort::Pushed)
-    //     .direction(params::Direction::Descending)
-    //     .per_page(100)
-    //     .page(1u32)
-    //     .send()
-    //     .await;
-
-    // let mut current_page_value = match current_page {
-    //     Ok(v) => v,
-    //     Err(e) => panic!("Error trying to get page: {}", e)
-    // };
-    //
-    // let prs = current_page_value.take_items();
-    //
-    // for pr in prs.iter() {
-    //     println!("Value: {}", pr.url.as_str());
-    // }
-}
+use clap::Parser;
+use harbor_cli::{Cli, commands, Commands, Error};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
+    let cli = Cli::parse();
 
-    let matches = get_matches();
-    if let Some(aws_account) = matches.get_one::<String>("account") {
-        println!("Account Number: {}", aws_account);
-        if let Some(env) = matches.get_one::<String>("env") {
-            println!("Environment: {}", env);
-            match matches.subcommand() {
-                Some(("start", _)) => {
-                    println!("Start matched, lets get it on");
-                    list_repos().await;
-                }
-                None => println!("Nothing"),
-                Some((&_, _)) => todo!()
-            }
+    match &cli.command {
+        Some(Commands::Enrich(enrich)) => commands::enrich::execute(enrich).await,
+        Some(Commands::Sbom(sbom)) => commands::sbom::execute(sbom).await,
+        _ => {
+            println!("command not found");
+            std::process::exit(1);
         }
     }
 }
---
+++ cli/src/commands/sbom.rs
@@ -0,0 +1,130 @@
+use std::str::FromStr;
+
+use clap::{Parser, ValueEnum};
+use clap::builder::PossibleValue;
+
+use crate::Error;
+
+/// The SBOM Command handler.
+pub async fn execute(args: &SbomArgs) -> Result<(), Error> {
+    match args.provider {
+        SbomProviderKind::FileSystem => {
+            FileSystemProvider::execute(&args.filesystem_args).await
+        }
+        SbomProviderKind::GitHub => {
+            GithubProvider::execute(&args.github_args).await
+        }
+        SbomProviderKind::Snyk => {
+            SnykProvider::execute(&args.snyk_args).await
+        }
+    }
+}
+
+/// Enumerates which SBOM generation strategy to employ.
+#[derive(Clone, Debug)]
+pub(crate) enum SbomProviderKind {
+    /// Generate a single SBOM from the filesystem using the default Syft provider.
+    FileSystem,
+    /// Generate one or more SBOMs from GitHub. Generation strategy depends on configuration options.
+    GitHub,
+    /// Generate one or more SBOMs from Snyk. Generation strategy depends on configuration options.
+    Snyk,
+}
+
+impl ValueEnum for SbomProviderKind {
+    fn value_variants<'a>() -> &'a [Self] {
+        &[Self::FileSystem, Self::GitHub, Self::Snyk]
+    }
+
+    fn to_possible_value(&self) -> Option<PossibleValue> {
+        Some(match self {
+            SbomProviderKind::FileSystem => {
+                PossibleValue::new("filesystem").help("Use the default SBOM Provider to generate an SBOM from the local filesystem")
+            }
+            SbomProviderKind::GitHub => {
+                PossibleValue::new("github").help("Use the GitHub SBOM Provider to generate one or more SBOMs from the GitHub API")
+            }
+            SbomProviderKind::Snyk => {
+                PossibleValue::new("github").help("Use the Snyk SBOM Provider to generate one or more SBOMs from the Snyk API")
+            }
+        })
+    }
+}
+
+/// Specifies the CLI args for the SBOM command.
+#[derive(Debug, Parser)]
+pub struct SbomArgs {
+    /// Specifies with SBOM Provider to invoke.
+    #[arg(short, long)]
+    provider: SbomProviderKind,
+
+    /// Flattened args for use with the file system SBOM provider.
+    #[command(flatten)]
+    pub filesystem_args: Option<FileSystemArgs>,
+
+    /// Flattened args for use with the GitHub SBOM provider.
+    #[command(flatten)]
+    pub github_args: Option<GitHubArgs>,
+
+    /// Flattened args for use with the Snyk SBOM provider.
+    #[command(flatten)]
+    pub snyk_args: Option<SnykArgs>,
+}
+
+/// Args for generating and SBOM from the filesystem.
+#[derive(Clone, Debug, Parser)]
+pub struct FileSystemArgs {}
+
+/// Args for generating one ore more SBOMs from a GitHub Organization.
+#[derive(Clone, Debug, Parser)]
+pub struct GitHubArgs {}
+
+/// Args for generating one or more SBOMs from the Snyk API.
+#[derive(Clone, Debug, Parser)]
+pub struct SnykArgs {}
+
+impl FromStr for SbomProviderKind {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let value = s.to_lowercase();
+        let value = value.as_str();
+        match value {
+            "filesystem"| "f" => Ok(SbomProviderKind::FileSystem),
+            "github"| "gh" | "g" => Ok(SbomProviderKind::GitHub),
+            "snyk" | "s" => Ok(SbomProviderKind::Snyk),
+            _ => Err(()),
+        }
+    }
+}
+
+
+/// Strategy pattern implementation that handles Snyk SBOM commands.
+struct FileSystemProvider {}
+
+impl FileSystemProvider {
+    async fn execute(_args: &Option<FileSystemArgs>) -> Result<(), Error> {
+        // Construct and invoke Core Services here or if args are contextual call specialized subroutine.
+        todo!()
+    }
+}
+
+/// Strategy pattern implementation that handles Snyk SBOM commands.
+struct GithubProvider {}
+
+impl GithubProvider {
+    async fn execute(_args: &Option<GitHubArgs>) -> Result<(), Error> {
+        // Construct and invoke Core Services here or if args are contextual call specialized subroutine.
+        todo!()
+    }
+}
+
+/// Strategy pattern implementation that handles Snyk SBOM commands.
+struct SnykProvider {}
+
+impl SnykProvider {
+    async fn execute(_args: &Option<SnykArgs>) -> Result<(), Error> {
+        // Construct and invoke Core Services here or if args are contextual call specialized subroutine.
+        todo!()
+    }
+}
