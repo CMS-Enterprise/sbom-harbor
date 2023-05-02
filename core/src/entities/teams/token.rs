a37236cd24ec407035cfba9a2c8904276a89ac29
Derek Strickland <derek.strickland@gmail.com>
2023-05-02 11:59:49 -0400

    Refactor Team models to entities

---
+++ core/src/entities/teams/token.rs
@@ -0,0 +1,50 @@
+use crate::Error;
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+/// A Token is an entity that represents a string used to authorize sending
+/// SBOMs into the system.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Token {
+    /// The unique identifier for the Token.
+    pub id: String,
+    /// The name of the token.
+    pub name: String,
+    /// The secret token value.
+    pub token: String,
+    /// Flag indicating whether the token is enabled.
+    pub enabled: bool,
+    /// The string representation of the expiration date of the token.
+    pub expires: String,
+}
+
+impl Token {
+    /// Constructor function for creating new [Token] instances.
+    pub fn new(name: String, expires: String, enabled: Option<bool>) -> Self {
+        Self {
+            id: "".to_string(),
+            name,
+            token: Uuid::new_v4().to_string(),
+            enabled: enabled.unwrap_or(false),
+            expires,
+        }
+    }
+
+    /// Determines whether a token is expired.
+    #[allow(dead_code)]
+    pub(crate) fn expired(&self) -> Result<bool, Error> {
+        if self.expires.is_empty() {
+            return Ok(false);
+        }
+
+        match DateTime::parse_from_rfc3339(&self.expires) {
+            Ok(expiry) => Ok(Utc::now() >= expiry),
+            Err(e) => Err(Error::InvalidFormat(format!(
+                "error parsing token expires: {}",
+                e
+            ))),
+        }
+    }
+}
---
+++ core/src/services/teams/service.rs
@@ -0,0 +1,22 @@
+use platform::mongodb::{Context, Service};
+
+use crate::entities::teams::Team;
+
+/// Contains all domain and transaction logic related to [Teams] and their subordinate entities.
+#[derive(Debug)]
+pub struct TeamService {
+    cx: Context,
+}
+
+impl TeamService {
+    /// Factory method to create new instance of type.
+    pub fn new(cx: Context) -> TeamService {
+        TeamService { cx }
+    }
+}
+
+impl Service<Team> for TeamService {
+    fn cx(&self) -> &Context {
+        &self.cx
+    }
+}
