40ba4b2452097e2bb09fcb292dbec6329902f9ac
Derek Strickland <derek.strickland@gmail.com>
2023-05-02 12:10:44 -0400

    Update core errors, config, modules, and test files

--- core/src/config.rs
+++ core/src/config.rs
@@ -1,13 +1,107 @@
 use crate::Error;
 use platform::config::from_env;
-use platform::mongodb::Context;
+use platform::mongodb::{Context, ContextKind};
+use std::str::FromStr;
 
-/// Returns the Mongo Connection URI as an environment variable. Defaults to local dev environment if not set.
-pub fn db_connection() -> Result<Context, Error> {
-    let mut cx: Context = from_env("DB_CONNECTION")?;
+enum Environ {
+    Local,
+    CI,
+    Dev,
+    Prod,
+}
 
-    cx.db_name = "harbor".to_string();
-    cx.key_name = "id".to_string();
+impl FromStr for Environ {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let environ = s.to_lowercase();
+        match environ.as_str() {
+            "local" => Ok(Self::Local),
+            "ci" => Ok(Self::CI),
+            "dev" => Ok(Self::Dev),
+            "prod" => Ok(Self::Prod),
+            _ => Ok(Self::Local),
+        }
+    }
+}
+
+/// Returns a Mongo Context based on environment. Defaults to local dev environment if not set.
+/// Optionally accepts a DB name. Defaults to `harbor`. Useful for overriding in tests.
+pub fn mongo_context(db_name: Option<&str>) -> Result<Context, Error> {
+    let db_name = match db_name {
+        None => "harbor",
+        Some(db_name) => db_name,
+    };
+
+    let mut cx = Context {
+        kind: ContextKind::Mongo,
+        host: "".to_string(),
+        username: "".to_string(),
+        password: "".to_string(),
+        port: 0,
+        db_name: db_name.to_string(),
+        key_name: "id".to_string(),
+        region: None,
+        cluster_name: None,
+        account_name: None,
+    };
+
+    build_context(&mut cx)?;
 
     Ok(cx)
+}
+
+fn build_context(cx: &mut Context) -> Result<(), Error> {
+    let environ = environment();
+
+    match environ {
+        Environ::Local => {
+            cx.host = "localhost".to_string();
+            cx.port = 27017;
+            cx.username = "root".to_string();
+            cx.password = "harbor".to_string();
+        }
+        Environ::CI => {
+            cx.host = "mongo".to_string();
+            cx.port = 27017;
+            cx.username = "root".to_string();
+            cx.password = "harbor".to_string();
+        }
+        Environ::Dev | Environ::Prod => {
+            cx.port = 27017;
+            cx.username = from_env("DOCDB_USERNAME").expect("username required");
+            cx.password = from_env("DOCDB_PASSWORD").expect("password required");
+            cx.cluster_name = Some(from_env("DOCDB_CLUSTER_NAME").expect("cluster name required"));
+            cx.region = Some(from_env("DOCDB_REGION").expect("region required"));
+        }
+    };
+
+    Ok(())
+}
+
+/// Returns the runtime environment from environment variables. Defaults to [Environ::Local].
+fn environment() -> Environ {
+    match from_env("ENVIRONMENT") {
+        None => Environ::Local,
+        Some(environ) => match Environ::from_str(environ.as_str()) {
+            Ok(e) => e,
+            Err(_) => Environ::Local,
+        },
+    }
+}
+
+/// Returns the Snyk API token from an environment variable.
+pub fn snyk_token() -> Result<String, Error> {
+    match from_env("SNYK_TOKEN") {
+        None => Err(Error::Config("Snyk token not set".to_string())),
+        Some(v) => Ok(v),
+    }
+}
+
+/// Returns the Harbor S3 bucket name.
+pub fn harbor_bucket() -> Result<String, Error> {
+    match from_env("HARBOR_FILE_STORE") {
+        None => Err(Error::Config("Harbor bucket not set".to_string())),
+        Some(v) => Ok(v),
+    }
 }
--- core/src/errors.rs
+++ core/src/errors.rs
@@ -6,19 +6,49 @@ pub enum Error {
     /// Configuration error.
     #[error("config error: {0}")]
     Config(String),
+    /// Entity error.
+    #[error("entity error: {0}")]
+    Entity(String),
+    /// Enrichment provider error.
+    #[error("enrichment provider error: {0}")]
+    Enrichment(String),
+    /// Finding provider error.
+    #[error("finding provider error: {0}")]
+    Finding(String),
     /// Invalid format.
     #[error("invalid format: {0}")]
     InvalidFormat(String),
     /// Error during db migrations.
     #[error("migrations error: {0}")]
     Migrations(String),
+    /// Error calling remote resource.
+    #[error("remote error: {0}")]
+    Remote(String),
     /// Runtime error.
     #[error("runtime error: {0}")]
     Runtime(String),
+    /// Sbom provider error.
+    #[error("sbom provider error: {0}")]
+    Sbom(String),
+    /// Scan provider error.
+    #[error("scan provider error: {0}")]
+    Scan(String),
+    /// Serialization error.
+    #[error("serialization error: {0}")]
+    Serde(String),
+    /// Snyk provider error.
+    #[error("snyk error: {0}")]
+    Snyk(String),
 }
 
 impl From<platform::Error> for Error {
     fn from(value: platform::Error) -> Self {
+        Error::Runtime(format!("{:?}", value))
+    }
+}
+
+impl From<platform::hyper::Error> for Error {
+    fn from(value: platform::hyper::Error) -> Self {
         Error::Runtime(format!("{:?}", value))
     }
 }
