9d326943074eb7173a1b69866fa6fad7e3fe72df
Derek Strickland <derek.strickland@gmail.com>
2023-06-15 11:29:26 -0400

    chore: fix linter warnings

--- core/src/services/analytics/sboms/provider.rs
+++ core/src/services/analytics/sboms/provider.rs
@@ -1,26 +1,23 @@
+use crate::entities::tasks::{Task, TaskStatus};
+use crate::services::analytics::sboms::service::AnalyticService;
+use crate::services::tasks::TaskProvider;
+use crate::Error;
+use async_trait::async_trait;
+use platform::mongodb::{Service, Store};
 use std::collections::HashMap;
 use std::fmt::Debug;
 use std::sync::Arc;
-use platform::mongodb::{Service, Store};
-use crate::entities::tasks::{Task, TaskStatus};
-use crate::Error;
-use crate::services::analytics::sboms::service::AnalyticService;
-use crate::services::tasks::TaskProvider;
-use async_trait::async_trait;
 
 /// AnalyticExecutionTask to add errors to the data store
 #[derive(Debug)]
 pub struct SbomDetailTask {
-    service: AnalyticService
+    service: AnalyticService,
 }
 
 impl SbomDetailTask {
-
     /// Creates a new AnalyticExecutionTask
     pub fn new(service: AnalyticService) -> Self {
-        Self {
-            service
-        }
+        Self { service }
     }
 }
 
@@ -32,36 +29,35 @@ impl Service<Task> for SbomDetailTask {
 
 #[async_trait]
 impl TaskProvider for SbomDetailTask {
-
     async fn run(&self, task: &mut Task) -> Result<HashMap<String, String>, Error> {
-
         let mut errors = HashMap::<String, String>::new();
         let mut report_paths = Vec::<String>::new();
 
         let primary_purls = match self.service.get_primary_purls().await {
             Ok(opt) => match opt {
                 Some(purls) => purls,
-                None => return Err(
-                    Error::Analytic(
-                        format!("Error attempting to get primary purls, none found!")
-                    )
-                ),
+                None => {
+                    return Err(Error::Analytic(
+                        "Error attempting to get primary purls, none found!".to_string(),
+                    ))
+                }
             },
-            Err(err) => return Err(
-                Error::Analytic(
-                    format!("Error attempting to get primary purls: {err}")
-                )
-            )
+            Err(err) => {
+                return Err(Error::Analytic(format!(
+                    "Error attempting to get primary purls: {err}"
+                )))
+            }
         };
 
         task.count = primary_purls.len() as u64;
 
         for purl in primary_purls {
             match self.service.generate_detail(purl.clone()).await {
-                Ok(file_path_option) =>
+                Ok(file_path_option) => {
                     if let Some(file_path) = file_path_option {
                         report_paths.push(file_path)
-                    },
+                    }
+                }
                 Err(err) => {
                     errors.insert(purl, format!("{}", err));
                 }
@@ -73,4 +69,3 @@ impl TaskProvider for SbomDetailTask {
         Ok(errors)
     }
 }
-
