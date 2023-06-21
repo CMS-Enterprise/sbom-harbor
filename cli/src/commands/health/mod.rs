3ca98fd6b572e8e92fd91ba1b58de8441e191eaf
Richard Jones <richard.jones@aquia.io>
2023-06-21 16:03:50 -0600

    now checking for snyk token; db connection, and s3 connection

--- cli/src/commands/health/mod.rs
+++ cli/src/commands/health/mod.rs
@@ -1,29 +1,43 @@
 use crate::Error;
 use clap::Parser;
-use platform::persistence::mongodb::Store;
+use harbcore::config::*;
+use platform::persistence::mongodb::client_from_context;
+use platform::persistence::s3::Store;
 
 /// The CommandFactory function for the `health` command.
 pub async fn execute(args: &HealthArgs) -> Result<(), Error> {
-    let cx = match &args.debug {
-        false => harbcore::config::harbor_context().map_err(|e| Error::Config(e.to_string()))?,
-        true => harbcore::config::dev_context(None).map_err(|e| Error::Config(e.to_string()))?,
-    };
-
-    match Store::new(&cx)
-        .await
-        .map_err(|e| Error::Enrich(e.to_string()))
-    {
-        Ok(_) => {
-            println!("Mongo: OK");
-            Ok(())
-        }
+    // ensure snyk token is set
+    match snyk_token() {
+        Ok(_) => {}
         Err(e) => {
-            println!("Mongo: cannot connect");
-            Err(e)
+            return Err(Error::System(e.to_string()));
         }
     }
 
-    // Ok(())
+    // ensure db is reachable
+    let cx = match &args.debug {
+        false => harbor_context().map_err(|e| Error::Config(e.to_string()))?,
+        true => dev_context(None).map_err(|e| Error::Config(e.to_string()))?,
+    };
+
+    let client = client_from_context(&cx).await?;
+    match client.list_database_names(None, None).await {
+        Ok(_) => {}
+        Err(e) => {
+            return Err(Error::System(e.to_string()));
+        }
+    }
+
+    // ensure s3 bucket is reachable
+    let s3_store = Store::new_from_env().await?;
+    let bucket_name = harbor_bucket().map_err(|e| Error::Config(e.to_string()))?;
+    match s3_store.list(bucket_name).await {
+        Ok(_) => {}
+        Err(e) => {
+            return Err(Error::System(e.to_string()));
+        }
+    }
+    Ok(())
 }
 /// Specifies the CLI args for the `health` command.
 #[derive(Debug, Parser)]
