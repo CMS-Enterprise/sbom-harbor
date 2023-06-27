068646282d39d32c1c5c51583e36acc605342a96
Derek Strickland <derek.strickland@gmail.com>
2023-06-27 19:51:56 -0400

    feat: add string hashing and verification functions.

--- sdk/platform/src/cryptography/mod.rs
+++ sdk/platform/src/cryptography/mod.rs
@@ -1,2 +1,5 @@
 /// Contains convenience functions for using the SHA-256 algorithm.
 pub mod sha256;
+
+/// Contains convenience functions for using the Argon2 secure key derivation function.
+pub mod argon2;
