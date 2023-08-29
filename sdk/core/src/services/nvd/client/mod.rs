a40a083a39a181922fac280f8cdbdeb7bf2be6e2
qtpeters <quinn.t.peters@gmail.com>
2023-08-29 08:39:51 -0400

    feat(nvd-construction-provider): got the NVD API client working with the models

--- sdk/core/src/services/nvd/client/mod.rs
+++ sdk/core/src/services/nvd/client/mod.rs
@@ -3,6 +3,7 @@ use platform::hyper::ContentType;
 use crate::services::nvd::client::models::NvdVulnerabilityV2;
 use platform::hyper::Client as HttpClient;
 use platform::hyper::token::Token;
+use crate::services::nvd::client::Error::NvdClientError;
 
 /// NVD response specific models
 mod models;
@@ -23,7 +24,7 @@ impl Client {
         }
     }
 
-    pub async fn get_page_of_vulnerabilities(&self) -> Result<Vec<NvdVulnerabilityV2>, Error> {
+    pub async fn get_page_of_vulnerabilities(&self) -> Result<NvdVulnerabilityV2, Error> {
 
         let start_index = 0;
         let results_per_page = 2;
@@ -31,25 +32,81 @@ impl Client {
         let url = format!("{}/?startIndex={}&resultsPerPage={}",
               BASE_URL, start_index, results_per_page);
 
-        let option: Option<Vec<NvdVulnerabilityV2>> = self
+        let option: Option<NvdVulnerabilityV2> = self
             .http_client
             .get(
                 url.as_str(),
                 ContentType::Json,
-                Some(Token::new(self.api_key.clone())),
+                Some(
+                    Token::new_with_header_name(
+                        String::from("apiKey"),
+                        self.api_key.clone()
+                    )
+                ),
                 None::<String>,
             )
             .await
             .map_err(Error::NvdResponse)?;
 
-        Ok(vec![])
+        match option {
+            None => Err(
+                NvdClientError(
+                    String::from("Response form NVD is empty")
+                )
+            ),
+            Some(nvd_response) => Ok(nvd_response),
+        }
     }
-
 }
 
 #[derive(Error, Debug)]
 pub enum Error {
     /// Error derived from our Http Client
     #[error(transparent)]
-    NvdResponse(#[from] platform::hyper::Error)
+    NvdResponse(#[from] platform::hyper::Error),
+
+    #[error("vulnerability provider error: {0}")]
+    NvdClientError(String)
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::{Map, Value};
+    use crate::config::nvd_api_key;
+    use crate::services::nvd::client::Client;
+    use crate::services::nvd::client::Error;
+    use platform::hyper::Client as HttpClient;
+    use crate::services::nvd::client::models::NvdVulnerabilityV2;
+
+
+    #[tokio::test]
+    async fn test_get_page_of_vulnerabilities() -> Result<(), Error> {
+        let key = nvd_api_key().unwrap();
+        let client = Client::new(HttpClient::new(), key);
+        let page = client.get_page_of_vulnerabilities().await?;
+        println!("{:#?}", page);
+        Ok(())
+    }
+
+    #[test]
+    fn json_test() {
+        let resp_body = "{\"resultsPerPage\":2,\"startIndex\":0,\"totalResults\":223594,\"format\":\"NVD_CVE\",\"version\":\"2.0\",\"timestamp\":\"2023-08-29T10:34:23.710\",\"vulnerabilities\":[{\"cve\":{\"id\":\"CVE-1999-0095\",\"sourceIdentifier\":\"cve@mitre.org\",\"published\":\"1988-10-01T04:00:00.000\",\"lastModified\":\"2019-06-11T20:29:00.263\",\"vulnStatus\":\"Modified\",\"descriptions\":[{\"lang\":\"en\",\"value\":\"The debug command in Sendmail is enabled, allowing attackers to execute commands as root.\"},{\"lang\":\"es\",\"value\":\"El comando de depuración de Sendmail está activado, permitiendo a atacantes ejecutar comandos como root.\"}],\"metrics\":{\"cvssMetricV2\":[{\"source\":\"nvd@nist.gov\",\"type\":\"Primary\",\"cvssData\":{\"version\":\"2.0\",\"vectorString\":\"AV:N\\/AC:L\\/Au:N\\/C:C\\/I:C\\/A:C\",\"accessVector\":\"NETWORK\",\"accessComplexity\":\"LOW\",\"authentication\":\"NONE\",\"confidentialityImpact\":\"COMPLETE\",\"integrityImpact\":\"COMPLETE\",\"availabilityImpact\":\"COMPLETE\",\"baseScore\":10.0},\"baseSeverity\":\"HIGH\",\"exploitabilityScore\":10.0,\"impactScore\":10.0,\"acInsufInfo\":false,\"obtainAllPrivilege\":true,\"obtainUserPrivilege\":false,\"obtainOtherPrivilege\":false,\"userInteractionRequired\":false}]},\"weaknesses\":[{\"source\":\"nvd@nist.gov\",\"type\":\"Primary\",\"description\":[{\"lang\":\"en\",\"value\":\"NVD-CWE-Other\"}]}],\"configurations\":[{\"nodes\":[{\"operator\":\"OR\",\"negate\":false,\"cpeMatch\":[{\"vulnerable\":true,\"criteria\":\"cpe:2.3:a:eric_allman:sendmail:5.58:*:*:*:*:*:*:*\",\"matchCriteriaId\":\"1D07F493-9C8D-44A4-8652-F28B46CBA27C\"}]}]}],\"references\":[{\"url\":\"http:\\/\\/seclists.org\\/fulldisclosure\\/2019\\/Jun\\/16\",\"source\":\"cve@mitre.org\"},{\"url\":\"http:\\/\\/www.openwall.com\\/lists\\/oss-security\\/2019\\/06\\/05\\/4\",\"source\":\"cve@mitre.org\"},{\"url\":\"http:\\/\\/www.openwall.com\\/lists\\/oss-security\\/2019\\/06\\/06\\/1\",\"source\":\"cve@mitre.org\"},{\"url\":\"http:\\/\\/www.osvdb.org\\/195\",\"source\":\"cve@mitre.org\"},{\"url\":\"http:\\/\\/www.securityfocus.com\\/bid\\/1\",\"source\":\"cve@mitre.org\"}]}},{\"cve\":{\"id\":\"CVE-1999-0082\",\"sourceIdentifier\":\"cve@mitre.org\",\"published\":\"1988-11-11T05:00:00.000\",\"lastModified\":\"2008-09-09T12:33:40.853\",\"vulnStatus\":\"Analyzed\",\"descriptions\":[{\"lang\":\"en\",\"value\":\"CWD ~root command in ftpd allows root access.\"}],\"metrics\":{\"cvssMetricV2\":[{\"source\":\"nvd@nist.gov\",\"type\":\"Primary\",\"cvssData\":{\"version\":\"2.0\",\"vectorString\":\"AV:N\\/AC:L\\/Au:N\\/C:C\\/I:C\\/A:C\",\"accessVector\":\"NETWORK\",\"accessComplexity\":\"LOW\",\"authentication\":\"NONE\",\"confidentialityImpact\":\"COMPLETE\",\"integrityImpact\":\"COMPLETE\",\"availabilityImpact\":\"COMPLETE\",\"baseScore\":10.0},\"baseSeverity\":\"HIGH\",\"exploitabilityScore\":10.0,\"impactScore\":10.0,\"acInsufInfo\":false,\"obtainAllPrivilege\":true,\"obtainUserPrivilege\":false,\"obtainOtherPrivilege\":false,\"userInteractionRequired\":false}]},\"weaknesses\":[{\"source\":\"nvd@nist.gov\",\"type\":\"Primary\",\"description\":[{\"lang\":\"en\",\"value\":\"NVD-CWE-Other\"}]}],\"configurations\":[{\"nodes\":[{\"operator\":\"OR\",\"negate\":false,\"cpeMatch\":[{\"vulnerable\":true,\"criteria\":\"cpe:2.3:a:ftp:ftp:*:*:*:*:*:*:*:*\",\"matchCriteriaId\":\"30D7F58F-4C55-4D19-984C-79B6C9525BEB\"},{\"vulnerable\":true,\"criteria\":\"cpe:2.3:a:ftpcd:ftpcd:*:*:*:*:*:*:*:*\",\"matchCriteriaId\":\"1D85A7F5-C187-4707-8681-F96A91F58318\"}]}]}],\"references\":[{\"url\":\"http:\\/\\/www.alw.nih.gov\\/Security\\/Docs\\/admin-guide-to-cracking.101.html\",\"source\":\"cve@mitre.org\"}]}}]}";
+        // let resp_body = "{}";
+        let result = match serde_json::from_slice(resp_body.as_ref()) {
+            Ok(r) => {
+                println!("Got Response: {:#?}", r);
+                r
+            },
+            Err(err) => {
+                println!("!!!! Error: {}", err);
+                Value::Object(Map::new())
+            }
+        };
+
+        let ds: Option<NvdVulnerabilityV2> = serde_json::from_value(result)
+            .expect("Error converting from value");
+
+        println!("{:#?}", ds);
+    }
+
 }
--- sdk/core/src/services/nvd/client/models.rs
+++ sdk/core/src/services/nvd/client/models.rs
@@ -1,6 +1,8 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use chrono::NaiveDateTime;
+use serde_json::Value;
+use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct NvdVulnerabilityV2 {
@@ -11,27 +13,28 @@ pub struct NvdVulnerabilityV2 {
     #[serde(rename = "totalResults")]
     total_results: i32,
     #[serde(rename = "format")]
-    format_str: String,
-    version: String,
-    timestamp: NaiveDateTime,
-    vulnerabilities: Vec<DefCveItem>,
+    format_str: Option<String>,
+    version: Option<String>,
+    timestamp: Option<NaiveDateTime>,
+    // vulnerabilities: Option<Vec<DefCveItem>>,
+    vulnerabilities: Option<Value>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct DefCveItem {
-    cve: CveItem,
+    cve: Option<CveItem>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CveItem {
-    id: String,
+    id: Option<String>,
     #[serde(rename = "sourceIdentifier")]
     source_identifier: Option<String>,
     #[serde(rename = "vulnStatus")]
     vuln_status: Option<String>,
-    published: NaiveDateTime,
+    published: Option<NaiveDateTime>,
     #[serde(rename = "lastModified")]
-    last_modified: NaiveDateTime,
+    last_modified: Option<NaiveDateTime>,
     #[serde(rename = "evaluatorComment")]
     evaluator_comment: Option<String>,
     #[serde(rename = "evaluatorSolution")]
@@ -46,8 +49,8 @@ pub struct CveItem {
     cisa_required_action: Option<String>,
     #[serde(rename = "cisaVulnerabilityName")]
     cisa_vulnerability_name: Option<String>,
-    descriptions: Vec<LangString>,
-    references: Vec<Reference>,
+    descriptions: Option<Vec<LangString>>,
+    references: Option<Vec<Reference>>,
     metrics: Option<Metrics>,
     weaknesses: Option<Vec<Weakness>>,
     configurations: Option<Vec<Config>>,
@@ -57,54 +60,54 @@ pub struct CveItem {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct LangString {
-    lang: String,
-    value: String,
+    lang: Option<String>,
+    value: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Reference {
-    url: String,
+    url: Option<String>,
     source: Option<String>,
     tags: Option<Vec<String>>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct VendorComment {
-    organization: String,
-    comment: String,
+    organization: Option<String>,
+    comment: Option<String>,
     #[serde(rename = "lastModified")]
-    last_modified: NaiveDateTime,
+    last_modified: Option<NaiveDateTime>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Weakness {
-    source: String,
+    source: Option<String>,
     #[serde(rename = "type")]
-    type_str: String,
-    description: Vec<LangString>,
+    type_str: Option<String>,
+    description: Option<Vec<LangString>>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
-    operator: String,
-    negate: Option<bool>,
-    nodes: Vec<Node>,
+    operator: Option<String>,
+    negate: Option<Option<bool>>,
+    nodes: Option<Vec<Node>>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Node {
-    operator: String,
+    operator: Option<String>,
     negate: Option<bool>,
     #[serde(rename = "cpeMatch")]
-    cpe_match: Vec<CpeMatch>,
+    cpe_match: Option<Vec<CpeMatch>>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CpeMatch {
-    vulnerable: bool,
-    criteria: String,
+    vulnerable: Option<bool>,
+    criteria: Option<String>,
     #[serde(rename = "matchCriteriaId")]
-    match_criteria_id: uuid::Uuid,
+    match_criteria_id: Option<Uuid>,
     #[serde(rename = "versionStartExcluding")]
     version_start_excluding: Option<String>,
     #[serde(rename = "versionStartIncluding")]
@@ -127,11 +130,11 @@ pub struct Metrics {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Cvss {
-    source: String,
+    source: Option<String>,
     #[serde(rename = "type")]
-    type_str: String,
+    type_str: Option<String>,
     #[serde(rename = "cvssData")]
-    cvss_data: HashMap<String, serde_json::Value>,
+    cvss_data: Option<HashMap<String, Value>>,
     #[serde(rename = "baseSeverity")]
     base_severity: Option<String>,
     #[serde(rename = "exploitabilityScore")]
@@ -154,21 +157,21 @@ pub struct Cvss {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CvssV31 {
-    license: Vec<String>,
+    license: Option<Vec<String>>,
     #[serde(rename = "$schema")]
-    schema: String,
-    title: String,
+    schema: Option<String>,
+    title: Option<String>,
     #[serde(rename = "type")]
-    type_: String,
-    definitions: HashMap<String, DefinitionTypeV31>,
-    properties: PropertiesV31,
-    required: Vec<String>,
+    type_: Option<String>,
+    definitions: Option<HashMap<String, DefinitionTypeV31>>,
+    properties: Option<PropertiesV31>,
+    required: Option<Vec<String>>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct DefinitionTypeV31 {
     #[serde(rename = "type")]
-    type_: String,
+    type_: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     enum_: Option<Vec<String>>,
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -179,64 +182,64 @@ pub struct DefinitionTypeV31 {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct PropertiesV31 {
-    version: PropertyV31,
-    vector_string: PropertyV31,
+    version: Option<PropertyV31>,
+    vector_string: Option<PropertyV31>,
     #[serde(rename = "attackVector")]
-    attack_vector: PropertyReferenceV31,
+    attack_vector: Option<PropertyReferenceV31>,
     #[serde(rename = "attackComplexity")]
-    attack_complexity: PropertyReferenceV31,
+    attack_complexity: Option<PropertyReferenceV31>,
     #[serde(rename = "privilegesRequired")]
-    privileges_required: PropertyReferenceV31,
+    privileges_required: Option<PropertyReferenceV31>,
     #[serde(rename = "userInteraction")]
-    user_interaction: PropertyReferenceV31,
-    scope: PropertyReferenceV31,
+    user_interaction: Option<PropertyReferenceV31>,
+    scope: Option<PropertyReferenceV31>,
     #[serde(rename = "confidentialityImpact")]
-    confidentiality_impact: PropertyReferenceV31,
+    confidentiality_impact: Option<PropertyReferenceV31>,
     #[serde(rename = "integrityImpact")]
-    integrity_impact: PropertyReferenceV31,
+    integrity_impact: Option<PropertyReferenceV31>,
     #[serde(rename = "availabilityImpact")]
-    availability_impact: PropertyReferenceV31,
-    base_score: PropertyReferenceV31,
-    base_severity: PropertyReferenceV31,
+    availability_impact: Option<PropertyReferenceV31>,
+    base_score: Option<PropertyReferenceV31>,
+    base_severity: Option<PropertyReferenceV31>,
     #[serde(rename = "exploitCodeMaturity")]
-    exploit_code_maturity: PropertyReferenceV31,
+    exploit_code_maturity: Option<PropertyReferenceV31>,
     #[serde(rename = "remediationLevel")]
-    remediation_level: PropertyReferenceV31,
+    remediation_level: Option<PropertyReferenceV31>,
     #[serde(rename = "reportConfidence")]
-    report_confidence: PropertyReferenceV31,
-    temporal_score: PropertyReferenceV31,
-    temporal_severity: PropertyReferenceV31,
+    report_confidence: Option<PropertyReferenceV31>,
+    temporal_score: Option<PropertyReferenceV31>,
+    temporal_severity: Option<PropertyReferenceV31>,
     #[serde(rename = "confidentialityRequirement")]
-    confidentiality_requirement: PropertyReferenceV31,
+    confidentiality_requirement: Option<PropertyReferenceV31>,
     #[serde(rename = "integrityRequirement")]
-    integrity_requirement: PropertyReferenceV31,
+    integrity_requirement: Option<PropertyReferenceV31>,
     #[serde(rename = "availabilityRequirement")]
-    availability_requirement: PropertyReferenceV31,
+    availability_requirement: Option<PropertyReferenceV31>,
     #[serde(rename = "modifiedAttackVector")]
-    modified_attack_vector: PropertyReferenceV31,
+    modified_attack_vector: Option<PropertyReferenceV31>,
     #[serde(rename = "modifiedAttackComplexity")]
-    modified_attack_complexity: PropertyReferenceV31,
+    modified_attack_complexity: Option<PropertyReferenceV31>,
     #[serde(rename = "modifiedPrivilegesRequired")]
-    modified_privileges_required: PropertyReferenceV31,
+    modified_privileges_required: Option<PropertyReferenceV31>,
     #[serde(rename = "modifiedUserInteraction")]
-    modified_user_interaction: PropertyReferenceV31,
+    modified_user_interaction: Option<PropertyReferenceV31>,
     #[serde(rename = "modifiedScope")]
-    modified_scope: PropertyReferenceV31,
+    modified_scope: Option<PropertyReferenceV31>,
     #[serde(rename = "modifiedConfidentialityImpact")]
-    modified_confidentiality_impact: PropertyReferenceV31,
+    modified_confidentiality_impact: Option<PropertyReferenceV31>,
     #[serde(rename = "modifiedIntegrityImpact")]
-    modified_integrity_impact: PropertyReferenceV31,
+    modified_integrity_impact: Option<PropertyReferenceV31>,
     #[serde(rename = "modifiedAvailabilityImpact")]
-    modified_availability_impact: PropertyReferenceV31,
-    environmental_score: PropertyReferenceV31,
-    environmental_severity: PropertyReferenceV31,
+    modified_availability_impact: Option<PropertyReferenceV31>,
+    environmental_score: Option<PropertyReferenceV31>,
+    environmental_severity: Option<PropertyReferenceV31>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct PropertyV31 {
     description: Option<String>,
     #[serde(rename = "type")]
-    type_: String,
+    type_: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     enum_: Option<Vec<String>>,
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -246,28 +249,28 @@ pub struct PropertyV31 {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct PropertyReferenceV31 {
     #[serde(rename = "$ref")]
-    ref_: String,
+    ref_: Option<String>,
 }
 
 /* CVSS 3.0 Schema Structs */
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CvssV30 {
-    license: Vec<String>,
+    license: Option<Vec<String>>,
     #[serde(rename = "$schema")]
-    schema: String,
-    title: String,
+    schema: Option<String>,
+    title: Option<String>,
     #[serde(rename = "type")]
-    type_: String,
-    definitions: HashMap<String, DefinitionTypeV30>,
-    properties: PropertiesV30,
-    required: Vec<String>,
+    type_: Option<String>,
+    definitions: Option<HashMap<String, DefinitionTypeV30>>,
+    properties: Option<PropertiesV30>,
+    required: Option<Vec<String>>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct DefinitionTypeV30 {
     #[serde(rename = "type")]
-    type_: String,
+    type_: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     enum_: Option<Vec<String>>,
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -278,64 +281,64 @@ pub struct DefinitionTypeV30 {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct PropertiesV30 {
-    version: PropertyV30,
-    vector_string: PropertyV30,
+    version: Option<PropertyV30>,
+    vector_string: Option<PropertyV30>,
     #[serde(rename = "attackVector")]
-    attack_vector: PropertyReferenceV30,
+    attack_vector: Option<PropertyReferenceV30>,
     #[serde(rename = "attackComplexity")]
-    attack_complexity: PropertyReferenceV30,
+    attack_complexity: Option<PropertyReferenceV30>,
     #[serde(rename = "privilegesRequired")]
-    privileges_required: PropertyReferenceV30,
+    privileges_required: Option<PropertyReferenceV30>,
     #[serde(rename = "userInteraction")]
-    user_interaction: PropertyReferenceV30,
-    scope: PropertyReferenceV30,
+    user_interaction: Option<PropertyReferenceV30>,
+    scope: Option<PropertyReferenceV30>,
     #[serde(rename = "confidentialityImpact")]
-    confidentiality_impact: PropertyReferenceV30,
+    confidentiality_impact: Option<PropertyReferenceV30>,
     #[serde(rename = "integrityImpact")]
-    integrity_impact: PropertyReferenceV30,
+    integrity_impact: Option<PropertyReferenceV30>,
     #[serde(rename = "availabilityImpact")]
-    availability_impact: PropertyReferenceV30,
-    base_score: PropertyReferenceV30,
-    base_severity: PropertyReferenceV30,
+    availability_impact: Option<PropertyReferenceV30>,
+    base_score: Option<PropertyReferenceV30>,
+    base_severity: Option<PropertyReferenceV30>,
     #[serde(rename = "exploitCodeMaturity")]
-    exploit_code_maturity: PropertyReferenceV30,
+    exploit_code_maturity: Option<PropertyReferenceV30>,
     #[serde(rename = "remediationLevel")]
-    remediation_level: PropertyReferenceV30,
+    remediation_level: Option<PropertyReferenceV30>,
     #[serde(rename = "reportConfidence")]
-    report_confidence: PropertyReferenceV30,
-    temporal_score: PropertyReferenceV30,
-    temporal_severity: PropertyReferenceV30,
+    report_confidence: Option<PropertyReferenceV30>,
+    temporal_score: Option<PropertyReferenceV30>,
+    temporal_severity: Option<PropertyReferenceV30>,
     #[serde(rename = "confidentialityRequirement")]
-    confidentiality_requirement: PropertyReferenceV30,
+    confidentiality_requirement: Option<PropertyReferenceV30>,
     #[serde(rename = "integrityRequirement")]
-    integrity_requirement: PropertyReferenceV30,
+    integrity_requirement: Option<PropertyReferenceV30>,
     #[serde(rename = "availabilityRequirement")]
-    availability_requirement: PropertyReferenceV30,
+    availability_requirement: Option<PropertyReferenceV30>,
     #[serde(rename = "modifiedAttackVector")]
-    modified_attack_vector: PropertyReferenceV30,
+    modified_attack_vector: Option<PropertyReferenceV30>,
     #[serde(rename = "modifiedAttackComplexity")]
-    modified_attack_complexity: PropertyReferenceV30,
+    modified_attack_complexity: Option<PropertyReferenceV30>,
     #[serde(rename = "modifiedPrivilegesRequired")]
-    modified_privileges_required: PropertyReferenceV30,
+    modified_privileges_required: Option<PropertyReferenceV30>,
     #[serde(rename = "modifiedUserInteraction")]
-    modified_user_interaction: PropertyReferenceV30,
+    modified_user_interaction: Option<PropertyReferenceV30>,
     #[serde(rename = "modifiedScope")]
-    modified_scope: PropertyReferenceV30,
+    modified_scope: Option<PropertyReferenceV30>,
     #[serde(rename = "modifiedConfidentialityImpact")]
-    modified_confidentiality_impact: PropertyReferenceV30,
+    modified_confidentiality_impact: Option<PropertyReferenceV30>,
     #[serde(rename = "modifiedIntegrityImpact")]
-    modified_integrity_impact: PropertyReferenceV30,
+    modified_integrity_impact: Option<PropertyReferenceV30>,
     #[serde(rename = "modifiedAvailabilityImpact")]
-    modified_availability_impact: PropertyReferenceV30,
-    environmental_score: PropertyReferenceV30,
-    environmental_severity: PropertyReferenceV30,
+    modified_availability_impact: Option<PropertyReferenceV30>,
+    environmental_score: Option<PropertyReferenceV30>,
+    environmental_severity: Option<PropertyReferenceV30>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct PropertyV30 {
     description: Option<String>,
     #[serde(rename = "type")]
-    type_: String,
+    type_: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     enum_: Option<Vec<String>>,
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -345,50 +348,50 @@ pub struct PropertyV30 {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct PropertyReferenceV30 {
     #[serde(rename = "$ref")]
-    ref_: String,
+    ref_: Option<String>,
 }
 
 /* CVSS 2.0 Schema Structs */
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CvssV20 {
-    pub version: String,
+    pub version: Option<String>,
     #[serde(rename = "vectorString")]
-    pub vector_string: String,
+    pub vector_string: Option<String>,
     #[serde(rename = "accessVector")]
-    pub access_vector: AccessVectorTypeV20,
+    pub access_vector: Option<AccessVectorTypeV20>,
     #[serde(rename = "accessComplexity")]
-    pub access_complexity: AccessComplexityTypeV20,
+    pub access_complexity: Option<AccessComplexityTypeV20>,
     #[serde(rename = "authentication")]
-    pub authentication: AuthenticationTypeV20,
+    pub authentication: Option<AuthenticationTypeV20>,
     #[serde(rename = "confidentialityImpact")]
-    pub confidentiality_impact: CiaTypeV20,
+    pub confidentiality_impact: Option<CiaTypeV20>,
     #[serde(rename = "integrityImpact")]
-    pub integrity_impact: CiaTypeV20,
+    pub integrity_impact: Option<CiaTypeV20>,
     #[serde(rename = "availabilityImpact")]
-    pub availability_impact: CiaTypeV20,
+    pub availability_impact: Option<CiaTypeV20>,
     #[serde(rename = "baseScore")]
-    pub base_score: ScoreTypeV20,
+    pub base_score: Option<ScoreTypeV20>,
     #[serde(rename = "exploitability")]
-    pub exploitability: ExploitabilityTypeV20,
+    pub exploitability: Option<ExploitabilityTypeV20>,
     #[serde(rename = "remediationLevel")]
-    pub remediation_level: RemediationLevelTypeV20,
+    pub remediation_level: Option<RemediationLevelTypeV20>,
     #[serde(rename = "reportConfidence")]
-    pub report_confidence: ReportConfidenceTypeV20,
+    pub report_confidence: Option<ReportConfidenceTypeV20>,
     #[serde(rename = "temporalScore")]
-    pub temporal_score: ScoreTypeV20,
+    pub temporal_score: Option<ScoreTypeV20>,
     #[serde(rename = "collateralDamagePotential")]
-    pub collateral_damage_potential: CollateralDamagePotentialTypeV20,
+    pub collateral_damage_potential: Option<CollateralDamagePotentialTypeV20>,
     #[serde(rename = "targetDistribution")]
-    pub target_distribution: TargetDistributionTypeV20,
+    pub target_distribution: Option<TargetDistributionTypeV20>,
     #[serde(rename = "confidentialityRequirement")]
-    pub confidentiality_requirement: CiaRequirementTypeV20,
+    pub confidentiality_requirement: Option<CiaRequirementTypeV20>,
     #[serde(rename = "integrityRequirement")]
-    pub integrity_requirement: CiaRequirementTypeV20,
+    pub integrity_requirement: Option<CiaRequirementTypeV20>,
     #[serde(rename = "availabilityRequirement")]
-    pub availability_requirement: CiaRequirementTypeV20,
+    pub availability_requirement: Option<CiaRequirementTypeV20>,
     #[serde(rename = "environmentalScore")]
-    pub environmental_score: ScoreTypeV20,
+    pub environmental_score: Option<ScoreTypeV20>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -485,9 +488,9 @@ pub enum CiaRequirementTypeV20 {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ScoreTypeV20 {
     #[serde(rename = "type")]
-    pub type_field: String,
+    pub type_field: Option<String>,
     #[serde(rename = "minimum")]
-    pub minimum: f64,
+    pub minimum: Option<f64>,
     #[serde(rename = "maximum")]
-    pub maximum: f64,
+    pub maximum: Option<f64>,
 }
